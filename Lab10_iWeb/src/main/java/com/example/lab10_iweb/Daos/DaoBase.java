37c0517c69cb7324b977b37e284ee8b22cec46f6
Gil Zanabria <108200371+xTarantino28@users.noreply.github.com>
2022-11-16 20:36:12 -0500

    dao base

--- Lab10_iWeb/src/main/java/com/example/lab10_iweb/Daos/DaoBase.java
+++ Lab10_iWeb/src/main/java/com/example/lab10_iweb/Daos/DaoBase.java
@@ -1,4 +1,20 @@
 package com.example.lab10_iweb.Daos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class DaoBase {
+public abstract  class DaoBase {
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }
+
+        String user = "root";
+        String pass = "123456";
+        String url = "jdbc:mysql://localhost:3306/bi_corp_business?serverTimeZone=America/Lima";
+
+        return DriverManager.getConnection(url, user, pass);
+    }
 }
