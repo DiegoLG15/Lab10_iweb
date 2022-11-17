9a9de31f85c66707fa82b1ce451092d2b8c0d411
Gil Zanabria <108200371+xTarantino28@users.noreply.github.com>
2022-11-17 18:19:47 -0500

    dao cliente  dao contrato

---
+++ Lab10_iWeb/src/main/java/com/example/lab10_iweb/Daos/DaoCliente.java
@@ -0,0 +1,84 @@
+package com.example.lab10_iweb.Daos;
+
+import com.example.lab10_iweb.Beans.Clientes;
+import java.sql.*;
+import java.util.ArrayList;
+
+
+
+
+public class DaoCliente extends DaoBase{
+
+    public ArrayList<Clientes> listarClientes(){
+
+        /*  QUERY PARA listarClientes
+        SELECT concat(clientes.g4093_name, ' ',coalesce(clientes.g4093_last_name," ")) AS `nombreCliente`, clientes.g4093_age AS 'edad', clientes.g4093_type AS 'tipoCliente', clientes.g4093_documentType AS 'tipoDocumento', count(contratos.g6789_cod_nup) AS 'cantidadContr'
+        FROM jm_client_bii clientes LEFT JOIN jm_cotr_bis contratos ON  (contratos.client_nro_id = clientes.g4093_nro_id)
+        GROUP BY `nombreCliente`*/
+
+        ArrayList<Clientes> listaClientes = new ArrayList<>();
+
+        try(Connection conn = getConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SSELECT * FROM bi_corp_business.jm_client_bii where g4093_nro_id not in (select nro_documento from credentials);")) {
+
+            while(rs.next()){
+                Clientes cliente = new Clientes();
+
+                cliente.setNumeroDocumento(rs.getString(1));
+                cliente.setNombreCliente(rs.getString(2));
+                cliente.setEdad(rs.getString(3));
+                if (rs.getString(4).equalsIgnoreCase("N")) {
+                    cliente.setTipoCliente("Natural");
+                } else if (rs.getString(4).equalsIgnoreCase("J")) {
+                    cliente.setTipoCliente("Jurídica");
+                }
+                cliente.setTipoDocumento(rs.getString(5));
+
+                listaClientes.add(cliente);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return listaClientes;
+    }
+
+
+
+    public Clientes buscarCliente(int idCliente) {
+
+        Clientes cliente = new Clientes();
+
+
+        try(Connection conn = getConnection();
+            PreparedStatement pstm = conn.prepareStatement("SELECT * FROM jm_client_bii WHERE g4093_nro_id=?;")){
+
+            pstm.setInt(1,idCliente);
+            try (ResultSet rs = pstm.executeQuery();){
+                if(rs.next()){
+                    cliente.setNumeroDocumento(rs.getString(1));
+                    cliente.setNombreCliente(rs.getString(2));
+                    cliente.setEdad(rs.getString(3));
+                    if (rs.getString(4).equalsIgnoreCase("N")) {
+                        cliente.setTipoCliente("Natural");
+                    } else if (rs.getString(4).equalsIgnoreCase("J")) {
+                        cliente.setTipoCliente("Jurídica");
+                    }
+                    cliente.setTipoDocumento(rs.getString(5));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return cliente;
+    }
+
+
+
+
+}
---
+++ Lab10_iWeb/src/main/java/com/example/lab10_iweb/Daos/DaoContrato.java
@@ -0,0 +1,116 @@
+package com.example.lab10_iweb.Daos;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+import com.example.lab10_iweb.Beans.Contratos;
+import com.example.lab10_iweb.Dtos.CantidadContratosDto;
+import com.example.lab10_iweb.Dtos.MaxExpectedLossDto;
+
+
+public class DaoContrato extends DaoBase {
+
+    public ArrayList<Contratos> listarContratos(int idCliente) {
+
+        ArrayList<Contratos> listaContratos = new ArrayList<>();
+
+        try(Connection conn = getConnection();
+            PreparedStatement pstm = conn.prepareStatement("SELECT * FROM bi_corp_business.jm_cotr_bis where client_nro_id = ?;")){
+
+            pstm.setInt(1,idCliente);
+            try (ResultSet rs = pstm.executeQuery();){
+                while(rs.next()){
+                    Contratos contrato = new Contratos();
+
+                    contrato.setNroDeContrato(rs.getString(1));
+                    contrato.setIdCliente(rs.getInt(2));
+                    if (rs.getInt(4) == 0) {
+                        contrato.setEstado("Normal");
+                        contrato.setMesesEnEseEstado(rs.getInt(5));
+                    } else if (rs.getInt(4) == 1) {
+                        contrato.setEstado("Cura");
+                        contrato.setMesesEnEseEstado(rs.getInt(5));
+                    } else if (rs.getInt(4) == 2) {
+                        contrato.setEstado("Mora");
+                        contrato.setMesesEnEseEstado(rs.getInt(5));
+                    }
+
+                    contrato.setDivisa(rs.getString(3));
+                    listaContratos.add(contrato);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return listaContratos;
+
+    }
+
+
+
+
+    public ArrayList<CantidadContratosDto> contratosCantidad(int idCliente) {
+
+        ArrayList<CantidadContratosDto> listaContratos = new ArrayList<>();
+
+        try(Connection conn = getConnection();
+            PreparedStatement pstm = conn.prepareStatement("SELECT G6789_status, count(g6789_contract) FROM bi_corp_business.jm_cotr_bis  contratos\n" +
+                    "left join bi_corp_business.jm_client_bii cliente on contratos.client_nro_id = cliente.g4093_nro_id\n" +
+                    "where cliente.g4093_nro_id = ? group by G6789_status order by G6789_status ASC ;")){
+
+            pstm.setInt(1,idCliente);
+            try (ResultSet rs = pstm.executeQuery();){
+                while(rs.next()){
+                    CantidadContratosDto cantidadContratosDto = new CantidadContratosDto();
+
+                    if (rs.getInt(1) == 0) {
+                        cantidadContratosDto.setEstado("Normal");
+                    } else if (rs.getInt(1) == 1) {
+                        cantidadContratosDto.setEstado("Cura");
+                    } else if (rs.getInt(1) == 2) {
+                        cantidadContratosDto.setEstado("Mora");
+                    }
+
+                    cantidadContratosDto.setCantidadPorEstado(rs.getInt(2));
+
+                    listaContratos.add(cantidadContratosDto);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return listaContratos;
+    }
+
+
+
+    public float getMaxExpectedLoss(int idCliente ) {
+        float maxexpectedloss = 0.0f;
+
+        try(Connection conn = getConnection();
+            PreparedStatement pstm = conn.prepareStatement("SELECT idJM_VALUES, max(valores.pd_value*valores.lgd_value*(1-valores.recovery_rate)),contratos.g6789_contract,cliente.g4093_nro_id   FROM bi_corp_business.jm_values  valores\n" +
+                    "left join bi_corp_business.jm_cotr_bis contratos on contratos.g6789_contract = valores.jm_cotr_bis_g6789_cod_nup\n" +
+                    "left join jm_client_bii cliente on contratos.client_nro_id = cliente.g4093_nro_id\n" +
+                    "where cliente.g4093_nro_id = ?;")){
+
+            pstm.setInt(1,idCliente);
+
+            try (ResultSet rs = pstm.executeQuery()){
+                if(rs.next()){
+                    MaxExpectedLossDto maxExpectedLossDto = new MaxExpectedLossDto();
+                    maxExpectedLossDto.setMaxExpectedLoss(rs.getFloat(2));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return maxexpectedloss;
+    }
+
+
+}
